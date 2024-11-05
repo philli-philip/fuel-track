aaa240f6f03b2ba5a746860d43791e21884bb53f
Philip <philip@mattha.net>
2024-11-05 22:15:38 +0100

    Persistent login

--- src/utils/supabase/supabase.ts
+++ src/utils/supabase/supabase.ts
@@ -6,13 +6,11 @@ const supabaseUrl = "https://enuzzkkzzjjmbnfqpwcq.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVudXp6a2t6empqbWJuZnFwd2NxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDc0NjQyOSwiZXhwIjoyMDQ2MzIyNDI5fQ.wDQePuTFBASP7zphYgvTLL01ONNDV8Hc1vnZQ1VQ7sU";
 
-console.log(supabaseAnonKey, supabaseUrl);
-
-export const supabase = createClient<Database>(supabaseUrl!, supabaseAnonKey!, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
-    persistSession: false,
+    persistSession: true,
     detectSessionInUrl: false,
   },
 });
