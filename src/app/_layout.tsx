75886ac9cf70c99b18aadc9b03cf2091f4929015
Philip <philip@mattha.net>
2024-11-07 10:05:43 +0100

    upgrade layout to typescript

---
+++ src/app/_layout.tsx
@@ -0,0 +1,44 @@
+import { ThemeContext, darkColors, lightColors } from "../utils/colors/colors";
+import { router, Stack, useRootNavigationState } from "expo-router";
+import { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
+import { supabase } from "../utils/supabase/supabase";
+import { Session } from "@supabase/supabase-js";
+
+export default function RootLayout() {
+  const color = useColorScheme();
+  const [session, setSession] = useState<Session | null>(null);
+  const rootNavigationState = useRootNavigationState();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    if (rootNavigationState?.key) {
+      if (session) router.replace("/dashboard");
+      if (!session) router.replace("/login");
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={color === "dark" ? darkColors : lightColors}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="dashboard" />
+        <Stack.Screen name="login" />
+        <Stack.Screen
+          name="newEntry"
+          options={{
+            headerShown: false,
+            animation: "fade_from_bottom",
+            presentation: "formSheet",
+          }}
+        />
+      </Stack>
+    </ThemeContext.Provider>
+  );
+}
