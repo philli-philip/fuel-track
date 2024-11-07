0fa3e2339cc774c7d9fcb6632dc0ce26999a9518
Philip <philip@mattha.net>
2024-11-07 11:49:51 +0100

    add logout button

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,10 +1,11 @@
-import { Button, SafeAreaViewBase, StyleSheet, Text, View } from "react-native";
-import { LinkButton } from "../components/button/button";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useContext, useEffect, useState } from "react";
 import { supabase } from "../utils/supabase/supabase";
 import { Loading } from "../components/Dashboard/loading";
-import { ThemeContext } from "../utils/colors/colors";
-import { router } from "expo-router";
+import { Theme, ThemeContext } from "../utils/colors/colors";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
   const colors = useContext(ThemeContext);
@@ -16,35 +17,7 @@ export default function Page() {
   const [totalCost, setTotalCost] = useState(0);
   const [averagePrice, setAveragePrice] = useState(0);
 
-  const styles = StyleSheet.create({
-    label: {
-      fontSize: 14,
-      color: colors.text.secondary,
-    },
-    group: {
-      display: "flex",
-      flexDirection: "column",
-      gap: 4,
-      width: "50%",
-    },
-    value: {
-      fontWeight: 700,
-      fontSize: 32,
-      color: colors.text.primary,
-    },
-    row: {
-      flexDirection: "row",
-      display: "flex",
-      paddingBottom: 48,
-    },
-    container: {
-      flexDirection: "column",
-      flex: 1,
-      paddingHorizontal: 24,
-      paddingTop: 0,
-      backgroundColor: colors.bg.default,
-    },
-  });
+  const styles = styling(colors);
 
   useEffect(() => {
     getData();
@@ -74,16 +47,21 @@ export default function Page() {
       setAveragePrice(totalCost / totalFueled);
       setPricePer1(totalCost / trackedDistance);
       setPricePer100((totalCost / trackedDistance) * 100);
-
-      setLoading(false);
     }
+
+    setLoading(false);
   };
 
   if (isLoading) {
     return <Loading />;
   } else {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.bar}>
+          <TouchableOpacity onPress={handleLogout}>
+            <MaterialIcons name="logout" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
         <View style={styles.row}>
           <View style={styles.group}>
             <Text style={styles.label}>Price per 100km</Text>
@@ -114,15 +92,65 @@ export default function Page() {
             <Text style={styles.value}>{averagePrice.toFixed(2)} €/litre</Text>
           </View>
         </View>
-        <LinkButton
-          href="/newEntry"
-          className="absolute right-8 bottom-32"
-          size="md"
-        >
-          <Text>New entry</Text>
-        </LinkButton>
-        <Button onPress={handleLogout} title="Logout" />
-      </View>
+        <Link href="/newEntry" asChild>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={{ color: colors.text.inverted }}>New entry</Text>
+          </TouchableOpacity>
+        </Link>
+      </SafeAreaView>
     );
   }
 }
+
+const styling = (theme: Theme) =>
+  StyleSheet.create({
+    btn: {
+      position: "absolute",
+      bottom: 48,
+      right: 16,
+      paddingHorizontal: 24,
+      paddingVertical: 16,
+      borderRadius: 12,
+      backgroundColor: theme.bg.accent,
+    },
+    btnDefault: {
+      opacity: 1,
+    },
+    btnPressed: {
+      opacity: 0.5,
+    },
+    label: {
+      fontSize: 14,
+      color: theme.text.secondary,
+    },
+    group: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 4,
+      width: "50%",
+    },
+    value: {
+      fontWeight: 700,
+      fontSize: 32,
+      color: theme.text.primary,
+    },
+    row: {
+      flexDirection: "row",
+      display: "flex",
+      paddingBottom: 72,
+    },
+    container: {
+      flexDirection: "column",
+      flex: 1,
+      paddingHorizontal: 24,
+      paddingTop: 24,
+      backgroundColor: theme.bg.default,
+      position: "relative",
+    },
+    bar: {
+      paddingBottom: 24,
+      paddingHorizontal: 16,
+      flexDirection: "row",
+      justifyContent: "flex-end",
+    },
+  });
