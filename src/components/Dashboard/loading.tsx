25255cea24b80f03df7b758b859c70c90681b8e7
Philip Mattha <philip@mattha.net>
2024-11-10 21:34:46 +0100

    add proper skeleton

--- src/components/Dashboard/loading.tsx
+++ src/components/Dashboard/loading.tsx
@@ -1,7 +1,49 @@
-import { View, Text } from "react-native";
+import { HStack } from "@/src/utils/hstack";
+import { VStack } from "@/src/utils/vstack";
+import React from "react";
+import { View, ViewProps } from "react-native";
 
 export const Loading = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Loading...</Text>
-  </View>
+  <VStack className="dark:bg-neutral-950 bg-gray-100 flex-1 px-6 gap-12">
+    <HStack space="2xl">
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/3 h-4" />
+        <Skeleton className="w-2/3 h-12" />
+      </VStack>
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/4 h-4" />
+        <Skeleton className="w-1/2 h-12" />
+      </VStack>
+    </HStack>
+    <HStack space="2xl">
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/3 h-4" />
+        <Skeleton className="w-3/4 h-12" />
+      </VStack>
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/3 h-4" />
+        <Skeleton className="w-2/3 h-12" />
+      </VStack>
+    </HStack>
+    <HStack space="2xl">
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/2 h-4" />
+        <Skeleton className="w-2/3 h-12" />
+      </VStack>
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/3 h-4" />
+        <Skeleton className="w-2/3 h-12" />
+      </VStack>
+    </HStack>
+  </VStack>
 );
+
+type SkeletonType = ViewProps & { className?: string };
+
+export const Skeleton = ({ className }: SkeletonType) => {
+  return (
+    <View
+      className={`rounded-lg animate-pulse bg-gray-100 dark:bg-gray-800 ${className}`}
+    />
+  );
+};
