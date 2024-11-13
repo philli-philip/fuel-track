6a4838f849473b8c3745e07802c4c5ab1c4efa7d
Philip Mattha <philip@mattha.net>
2024-11-13 18:54:15 +0100

    add keys to skeletons to avoid warnings for animated items

--- src/components/Dashboard/loading.tsx
+++ src/components/Dashboard/loading.tsx
@@ -7,32 +7,32 @@ export const Loading = () => (
   <VStack className="dark:bg-neutral-950 bg-gray-100 flex-1 px-6 gap-12">
     <HStack space="2xl">
       <VStack space="sm" className="flex-1">
-        <Skeleton className="w-1/3 h-4" />
-        <Skeleton className="w-2/3 h-12" />
+        <Skeleton className="w-1/3 h-4" key={"1-1"} />
+        <Skeleton className="w-2/3 h-12" key={"1-2"} />
       </VStack>
       <VStack space="sm" className="flex-1">
-        <Skeleton className="w-1/4 h-4" />
-        <Skeleton className="w-1/2 h-12" />
-      </VStack>
-    </HStack>
-    <HStack space="2xl">
-      <VStack space="sm" className="flex-1">
-        <Skeleton className="w-1/3 h-4" />
-        <Skeleton className="w-3/4 h-12" />
-      </VStack>
-      <VStack space="sm" className="flex-1">
-        <Skeleton className="w-1/3 h-4" />
-        <Skeleton className="w-2/3 h-12" />
+        <Skeleton className="w-1/4 h-4" key={"2-1"} />
+        <Skeleton className="w-1/2 h-12" key={"2-2"} />
       </VStack>
     </HStack>
     <HStack space="2xl">
       <VStack space="sm" className="flex-1">
-        <Skeleton className="w-1/2 h-4" />
-        <Skeleton className="w-2/3 h-12" />
+        <Skeleton className="w-1/3 h-4" key={"3-1"} />
+        <Skeleton className="w-3/4 h-12" key={"3-2"} />
       </VStack>
       <VStack space="sm" className="flex-1">
-        <Skeleton className="w-1/3 h-4" />
-        <Skeleton className="w-2/3 h-12" />
+        <Skeleton className="w-1/3 h-4" key={"4-1"} />
+        <Skeleton className="w-2/3 h-12" key={"4-2"} />
+      </VStack>
+    </HStack>
+    <HStack space="2xl">
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/2 h-4" key={"5-1"} />
+        <Skeleton className="w-2/3 h-12" key={"5-2"} />
+      </VStack>
+      <VStack space="sm" className="flex-1">
+        <Skeleton className="w-1/3 h-4" key={"6-1"} />
+        <Skeleton className="w-2/3 h-12" key={"6-2"} />
       </VStack>
     </HStack>
   </VStack>
