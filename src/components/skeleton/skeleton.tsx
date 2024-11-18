d165df812dcd9a98e39aea42625c2487f9d5a922
Philip Mattha <philip@mattha.net>
2024-11-18 14:25:59 +0100

    Skeleton with animation

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -1,4 +1,5 @@
-import { StyleProp, View, ViewProps, ViewStyle } from "react-native";
+import React from "react";
+import { Animated, StyleProp, View, ViewProps, ViewStyle } from "react-native";
 
 type SkeletonType = ViewProps & {
   className?: string;
@@ -6,5 +7,33 @@ type SkeletonType = ViewProps & {
 };
 
 export const Skeleton = ({ style }: SkeletonType) => {
-  return <View style={[{ borderRadius: 8, backgroundColor: "#eee" }, style]} />;
+  const pulse = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(pulse, {
+          toValue: 1,
+          duration: 700,
+          useNativeDriver: true,
+        }),
+        Animated.timing(pulse, {
+          toValue: 0,
+          duration: 700,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, [pulse]);
+  const pulseAnimation = pulse.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0.8],
+  });
+  return (
+    <View
+      style={[
+        { borderRadius: 8, backgroundColor: "#eee", opacity: pulse },
+        style,
+      ]}
+    />
+  );
 };
